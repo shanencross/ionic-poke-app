f7f2ef0b4abd845a51737db9f3b28cb6c53b96f7
Shanen Cross <shanencross@yahoo.com>
2021-10-12 12:57:41 -0700

    move state and function inside component and fix React typo

--- src/components/PokeDataControl.tsx
+++ src/components/PokeDataControl.tsx
@@ -1,16 +1,16 @@
 import PokeDataDisplay from "./PokeDataDisplay";
 import PokeSearchBar from "./PokeSearchBar"
-import { useState } from 'React';
+import { useState } from 'react';
 
 interface PokeDataControlProps { }
 
-const [searchInput, setSearchInput] = useState<string | null>(null);
-
-const handleSearchChange = (searchInput: string) => {
-  setSearchInput(searchInput);
-}
-
 const PokeDataControl: React.FC<PokeDataControlProps> = () => {
+  const [searchInput, setSearchInput] = useState<string | null>(null);
+
+  const handleSearchChange = (searchInput: string) => {
+    setSearchInput(searchInput);
+  }
+
   return (
     <div className="PokeDataControl">
       <PokeSearchBar onSearchChange={handleSearchChange}/>
