768dd5ccac9e527b9e8f71de2f100fe02a672685
Shanen Cross <shanencross@yahoo.com>
2021-10-15 12:15:40 -0700

    Update title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>PokeApp</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">PokeApp</IonTitle>
           </IonToolbar>
         </IonHeader>
         <PokeDataControl/>
