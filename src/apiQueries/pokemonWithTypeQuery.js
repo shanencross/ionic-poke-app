6b48613c85b8d5630790d61e05c890ec483c7fe5
Shanen Cross <shanencross@yahoo.com>
2021-10-16 05:32:58 -0700

    Use fetch to successfully handle API call

---
+++ src/apiQueries/pokemonWithTypeQuery.js
@@ -0,0 +1,28 @@
+const pokemonWithTypeQuery = `
+    query PokemonWithType($pokemon_type_input: String!) {
+      pokemon: pokemon_v2_pokemon (
+        where:  {
+          is_default : {
+            _eq: true 
+          }
+          pokemon_v2_pokemontypes: {
+            pokemon_v2_type: {
+              name: {
+                _iregex: $pokemon_type_input
+              }
+            }
+          }
+        }
+        order_by: { id: asc }
+      ) {
+        name
+        pokedex_number: pokemon_species_id
+        pokemon_types: pokemon_v2_pokemontypes {
+          pokemon_type: pokemon_v2_type {
+            name
+          }
+        }
+      }
+    }`;
+
+  export default pokemonWithTypeQuery;
--- src/components/PokeDataControl.tsx
+++ src/components/PokeDataControl.tsx
@@ -1,78 +1,32 @@
 import PokeDataDisplay from "./PokeDataDisplay";
 import PokeSearchBar from "./PokeSearchBar"
-// import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } from "graphql";
+import pokemonWithTypeQuery from "./../apiQueries/pokemonWithTypeQuery";
 
 interface PokeDataControlProps { }
 
 const PokeDataControl: React.FC<PokeDataControlProps> = () => {
-  // const [searchInput, setSearchInput] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState<string | null>(null);
 
   const handleSearchChange = (searchInput: string) => {
-    // setSearchInput(searchInput);
-    
-    const pokemonTypeObjectType = new GraphQLObjectType({
-      name: 'pokemonType',
-      fields: {
-        name: {
-          type: GraphQLString,
-          description: 'The name of the Pokemon type'
-        }
-      }
-    });
+    const variables = { "pokemon_type_input": searchInput };
 
-        const pokemonObjectType = new GraphQLObjectType({
-      name: 'pokemon',
-      fields: {
-        name: {
-          type: GraphQLString,
-          description: 'The name of the Pokemon'
-        },
-        pokedex_number: {
-          type: GraphQLInt,
-          description: 'The Pokedex entry number of the Pokemon'
-        },
-        pokemon_types: {
-          type: new GraphQLList(pokemonTypeObjectType),
-          description: 'List of Pokemon types'
-        },
+    fetch('https://beta.pokeapi.co/graphql/v1beta', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
       },
-    });
-
-    const schema = new GraphQLSchema({
-      query: new GraphQLObjectType({
-        name: 'PokemonWithType',
-        fields: {
-          pokemon: {
-            type: new GraphQLList(pokemonObjectType),
-            description: "List of pokemon with given type"
-          }
-        }
-      })
-    });
-
-    const source = `
-    query PokemonWithType($pokemontype: String!) {
-      pokemon: pokemon_v2_pokemon (
-        where:  { is_default : { _eq: true } pokemon_v2_pokemontypes: { pokemon_v2_type: { name: { _iregex: $pokemontype } } } } 
-        order_by: { id: asc }
-      ) {
-        name
-        pokedex_number: pokemon_species_id
-        pokemon_types: pokemon_v2_pokemontypes {
-          pokemon_type: pokemon_v2_type {
-            name
-          }
-        }
-      }
-    }`;
-    
-    const variableValues = { "pokemon_type": searchInput };
-
-    graphql({schema, source, variableValues}).then((result) => {
-      console.log(result);
-    });
+      body: JSON.stringify({ query: pokemonWithTypeQuery, variables })
+    })
+      .then(response => response.json())
+      .then(data => setSearchResults(data));
   }
+
+  useEffect(() => {
+    console.log(searchResults);
+  }, [searchResults]);
 
   return (
     <div className="PokeDataControl">
